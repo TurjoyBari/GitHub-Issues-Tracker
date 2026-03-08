f1b9bbfd0c03ab2cc8bd0f3f2140e4ac068f9598
TurjoyBari <bariturjo@gmail.com>
2026-03-08 15:53:01 +0600

    update Modal

--- script/script.js
+++ script/script.js
@@ -1,6 +1,4 @@
-
 let allIssues = [];
-
 
 const allToggleBtn = document.getElementById("all-toggle-btn");
 const openToggleBtn = document.getElementById("open-toggle-btn");
@@ -22,21 +20,140 @@ function toggleStyle(id) {
   selected.classList.add("bg-[#4A00FF]", "text-white");
 }
 
+const loadCardDetail = async (id) => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayCardDetail(details.data);
+};
+const displayCardDetail = (issue) => {
+  console.log(issue);
+  const detailsBox = document.getElementById("details-container");
 
+  function getLabelBadge(label) {
+    if (!label) {
+      return "";
+    }
+    if (label === "open") {
+      return `
+       <div class="badge badge-success text-[12px] text-white rounded-full">Opened</div>
+       `;
+    }
+    if (label === "closed") {
+      return `
+       <div class="badge badge-error text-[12px] text-white rounded-full">closed</div>
+       `;
+    }
 
+    if (label === "high") {
+      return `
+       <div class="badge badge-error text-[12px] text-white rounded-full uppercase">High</div>
+       `;
+    }
+    if (label === "medium") {
+      return `
+       <div class="badge badge-warning text-[12px] text-white rounded-full uppercase">medium</div>
+       `;
+    }
+    if (label === "low") {
+      return `
+       <div class="badge badge-neutral text-[12px] text-white rounded-full uppercase">low</div>
+       `;
+    }
+
+    if (label === "bug") {
+      return `
+       <div class="badge badge-soft badge-error uppercase">
+           <img src="./assets/Vector.png" alt="">
+           bug
+       </div>
+       `;
+    }
+
+    if (label === "help wanted") {
+      return `
+       <div class="badge badge-soft badge-warning uppercase">
+           <img src="./assets/Lifebuoy.png" alt="">
+           help wanted
+       </div>
+       `;
+    }
+
+    if (label === "enhancement") {
+      return `
+       <div class="badge badge-soft badge-success uppercase">
+           <img src="./assets/Lifebuoy.png" alt="">
+           enhancement
+       </div>
+       `;
+    }
+
+    if (label === "documentation") {
+      return `
+       <div class="badge badge-soft badge-success uppercase">
+           <img src="./assets/Lifebuoy.png" alt="">
+           documentation
+       </div>
+       `;
+    }
+
+    if (label === "good first issue") {
+      return `
+       <div class="badge badge-soft badge-success uppercase">
+           <img src="./assets/Lifebuoy.png" alt="">
+           good first issue
+       </div>
+       `;
+    }
+
+    return "";
+  }
+
+  detailsBox.innerHTML = `
+    
+            <div>
+                <h1 class="font-bold text-[24px] text-[#1F2937]">${issue.title}</h1>
+                <div class="flex gap-5 items-center pt-[8px]">
+                    ${getLabelBadge(issue.status)}
+                    <ul class="text-[#64748B] text-[12px] list-disc flex gap-5">
+                        <li>Opened by ${issue.author}</li>
+                        <li>22/02/2026</li>
+                    </ul>                    
+                </div>
+            </div>
+            <div>
+                <div class="card-actions pt-[12px] flex gap-2 flex-wrap">
+                    ${getLabelBadge(issue.labels[0])}
+                    ${getLabelBadge(issue.labels[1])}
+                </div>
+            </div>
+            <p class="text-[12px] text-[#64748B] line-clamp-2">
+                 ${issue.description}
+            </p>
+            <div class="flex justify-between bg-gray-100 p-4">
+                <div>
+                    <p class="text-[#64748B] text-[16px]">Assignee:</p>
+                    <h2 class="font-semibold text-[16px] text-[#1F2937]"> ${issue.assignee}</h2>
+                </div>
+                <div>
+                    <p class="text-[#64748B] text-[16px]">Priority:</p>
+                    ${getLabelBadge(issue.priority)}
+                </div>
+            </div>
+    
+    
+    
+    `;
+  document.getElementById("issue_modal").showModal();
+};
 
 const loadIssues = () => {
   fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((response) => response.json())
     .then((data) => {
-         allIssues = data.data;
-         displayIssues(allIssues);
-});
-
-
-    
-
-
+      allIssues = data.data;
+      displayIssues(allIssues);
+    });
 
   const displayIssues = (issues) => {
     console.log(issues);
@@ -49,100 +166,84 @@ const loadIssues = () => {
     issues.forEach((issue) => {
       const btnDiv = document.createElement("div");
 
+      let priorityBadge = "";
 
-    let priorityBadge = "";
+      if (issue.priority === "high") {
+        priorityBadge = `<div class="badge badge-soft badge-error">High</div>`;
+      } else if (issue.priority === "medium") {
+        priorityBadge = `<div class="badge badge-soft badge-warning">Medium</div>`;
+      } else {
+        priorityBadge = `<div class="badge badge-outline badge-info">Info</div>`;
+      }
 
-    if (issue.priority === "high") {
-      priorityBadge = `<div class="badge badge-soft badge-error">High</div>`;
-    } 
-    else if (issue.priority === "medium") {
-      priorityBadge = `<div class="badge badge-soft badge-warning">Medium</div>`;
-    } 
-    else {
-      priorityBadge = `<div class="badge badge-outline badge-info">Info</div>`;
-    }
+      function getLabelBadge(label) {
+        if (!label) {
+          return "";
+        }
 
-
-
-
-
-    function getLabelBadge(label) {
-        
-     if (!label) {
-       return ""; 
-     }
-    
-     if (label === "bug") {
-       return `
+        if (label === "bug") {
+          return `
        <div class="badge badge-soft badge-error uppercase">
            <img src="./assets/Vector.png" alt="">
            bug
        </div>
        `;
-     }
-    
-     if (label === "help wanted") {
-       return `
+        }
+
+        if (label === "help wanted") {
+          return `
        <div class="badge badge-soft badge-warning uppercase">
            <img src="./assets/Lifebuoy.png" alt="">
            help wanted
        </div>
        `;
-     }
-    
-     if (label === "enhancement") {
-       return `
+        }
+
+        if (label === "enhancement") {
+          return `
        <div class="badge badge-soft badge-success uppercase">
            <img src="./assets/Lifebuoy.png" alt="">
            enhancement
        </div>
        `;
-     }
-    
-     if (label === "documentation") {
-       return `
+        }
+
+        if (label === "documentation") {
+          return `
        <div class="badge badge-soft badge-success uppercase">
            <img src="./assets/Lifebuoy.png" alt="">
            documentation
        </div>
        `;
-     }
-    
-     if (label === "good first issue") {
-       return `
+        }
+
+        if (label === "good first issue") {
+          return `
        <div class="badge badge-soft badge-success uppercase">
            <img src="./assets/Lifebuoy.png" alt="">
            good first issue
        </div>
        `;
-     }
-    
-     return "";
+        }
 
-}
+        return "";
+      }
 
+      let statusImg = "";
+      let borderColor = "";
 
+      if (issue.status === "open") {
+        statusImg = "./assets/Open-Status.png";
+        borderColor = "#00A96E";
+      } else {
+        statusImg = "./assets/Closed-Status.png";
+        borderColor = "#A855F7";
+      }
 
+      btnDiv.innerHTML = `
 
-
-
-    let statusImg = "";
-    let borderColor = "";
-
-    if (issue.status === "open") {
-      statusImg = "./assets/Open-Status.png";
-      borderColor = "#00A96E";
-    } else {
-      statusImg = "./assets/Closed-Status.png";
-      borderColor = "#A855F7";
-    }
-
-
-
-    btnDiv.innerHTML = `
-
-        <div class="card p-4 h-full bg-base-100 w-full shadow-sm border-t-4"
-             style="border-top-color:${borderColor}">
+        <div class="card  p-4 h-full bg-base-100 w-full shadow-sm border-t-4" onclick="loadCardDetail(${issue.id})"
+             style="border-top-color:${borderColor} ">
 
             <div class="h-60 w-full">
 
@@ -177,6 +278,7 @@ const loadIssues = () => {
                     ${issue.createdAt}
                 </p>
             </div>
+            
 
         </div>
         `;
@@ -186,9 +288,7 @@ const loadIssues = () => {
   };
 };
 
-
 function displayIssues(issues) {
-
   const issueCount = document.getElementById("issueCount");
   issueCount.innerText = issues.length;
 
@@ -196,7 +296,6 @@ function displayIssues(issues) {
   cardContainer.innerHTML = "";
 
   issues.forEach((issue) => {
-
     const btnDiv = document.createElement("div");
 
     let statusImg = "";
@@ -210,92 +309,73 @@ function displayIssues(issues) {
       borderColor = "#A855F7";
     }
 
-
-
-        let priorityBadge = "";
+    let priorityBadge = "";
 
     if (issue.priority === "high") {
       priorityBadge = `<div class="badge badge-soft badge-error">High</div>`;
-    } 
-    else if (issue.priority === "medium") {
+    } else if (issue.priority === "medium") {
       priorityBadge = `<div class="badge badge-soft badge-warning">Medium</div>`;
-    } 
-    else {
+    } else {
       priorityBadge = `<div class="badge badge-outline badge-info">Info</div>`;
     }
 
-
-
-
     function getLabelBadge(label) {
-        
-     if (!label) {
-       return ""; 
-     }
-    
-     if (label === "bug") {
-       return `
+      if (!label) {
+        return "";
+      }
+
+      if (label === "bug") {
+        return `
        <div class="badge badge-soft badge-error uppercase">
            <img src="./assets/Vector.png" alt="">
            bug
        </div>
        `;
-     }
-    
-     if (label === "help wanted") {
-       return `
+      }
+
+      if (label === "help wanted") {
+        return `
        <div class="badge badge-soft badge-warning uppercase">
            <img src="./assets/Lifebuoy.png" alt="">
            help wanted
        </div>
        `;
-     }
-    
-     if (label === "enhancement") {
-       return `
+      }
+
+      if (label === "enhancement") {
+        return `
        <div class="badge badge-soft badge-success uppercase">
            <img src="./assets/Lifebuoy.png" alt="">
            enhancement
        </div>
        `;
-     }
-    
-     if (label === "documentation") {
-       return `
+      }
+
+      if (label === "documentation") {
+        return `
        <div class="badge badge-soft badge-success uppercase">
            <img src="./assets/Lifebuoy.png" alt="">
            documentation
        </div>
        `;
-     }
-    
-     if (label === "good first issue") {
-       return `
+      }
+
+      if (label === "good first issue") {
+        return `
        <div class="badge badge-soft badge-success uppercase">
            <img src="./assets/Lifebuoy.png" alt="">
            good first issue
        </div>
        `;
-     }
-    
-     return "";
+      }
 
-}
-
-
-
-
-    
-
-
-
-
-
+      return "";
+    }
 
     btnDiv.innerHTML = `
     
  
-        <div class="card p-4 h-full bg-base-100 w-full shadow-sm border-t-4"
+        <div class="card p-4 h-full bg-base-100 w-full shadow-sm border-t-4" onclick="loadCardDetail(${issue.id})"
              style="border-top-color:${borderColor}">
 
             <div class="h-60 w-full">
@@ -339,19 +419,13 @@ function displayIssues(issues) {
     `;
 
     cardContainer.appendChild(btnDiv);
-
   });
 }
 
 function filterIssues(status) {
-
-  const filtered = allIssues.filter(issue => issue.status === status);
+  const filtered = allIssues.filter((issue) => issue.status === status);
 
   displayIssues(filtered);
-
 }
-
-
-
 
 loadIssues();
